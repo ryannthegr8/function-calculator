21156d0d5f4f5d6aa5dea2e363216346981304d9
ryannthegr8 <mmbaleryan@gmail.com>
2022-02-15 09:07:48 +0300

    first adding of files

---
+++ js/script.js
@@ -0,0 +1,33 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+}
+
+clear() {
+
+}
+editout() {
+
+}
+appendNumber() {
+
+}
+chooseoperation() {
+
+}
+evaluate() {
+
+}
+
+
+
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allclearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
